e9d8f8e87c5b704fb7ed7b93577e2e16866b5136
Max Gonzih <gonzih@gmail.com>
2016-10-06 09:27:49 +0200

    Go with the loop inside the go programm

--- main.go
+++ main.go
@@ -6,24 +6,39 @@ import (
 	"log"
 	"net/http"
 	"os/exec"
+	"os/user"
 	"strconv"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/julienschmidt/httprouter"
 )
 
 var gpioMutex sync.Mutex
+var tempeHumidMutex sync.RWMutex
 
-func readTemperature() (float64, float64, error) {
-	cmd := exec.Command("/home/debian/bin/temperature.py")
+var temperature, humidity float64
+
+func readTemperature() error {
+	usr, err := user.Current()
+
+	if err != nil {
+		return fmt.Errorf("Error while getting info about current user: %s", err)
+	}
+
+	cmd := exec.Command(usr.HomeDir + "/bin/temperature.py")
 	var stdout, stderr bytes.Buffer
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
-	err := cmd.Run()
+
+	gpioMutex.Lock()
+	defer gpioMutex.Unlock()
+
+	err = cmd.Run()
 
 	if err != nil {
-		return 0, 0, err
+		return err
 	}
 
 	output := strings.Trim(stdout.String(), "\n")
@@ -32,22 +47,27 @@ func readTemperature() (float64, float64, error) {
 	log.Printf("%v", vals)
 
 	if len(vals) < 2 {
-		return 0, 0, fmt.Errorf("Can't parse output, stdout: '%s', stderr: '%s'", output, stderr.String())
+		return fmt.Errorf("Can't parse output, stdout: '%s', stderr: '%s'", output, stderr.String())
 	}
 
-	temperature, err := strconv.ParseFloat(vals[0], 64)
+	tempTemperature, err := strconv.ParseFloat(vals[0], 64)
 
 	if err != nil {
-		return 0, 0, err
+		return err
 	}
 
-	humidity, err := strconv.ParseFloat(vals[1], 64)
+	tempHumidity, err := strconv.ParseFloat(vals[1], 64)
 
 	if err != nil {
-		return 0, 0, err
+		return err
 	}
 
-	return temperature, humidity, nil
+	tempeHumidMutex.Lock()
+	defer tempeHumidMutex.Unlock()
+	temperature = tempTemperature
+	humidity = tempHumidity
+
+	return nil
 }
 
 func temperatureHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
@@ -56,18 +76,30 @@ func temperatureHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Par
 
 	w.Header().Add("Content-Type", "text/plain")
 
-	temperature, humidity, err := readTemperature()
+	tempeHumidMutex.RLock()
+	defer tempeHumidMutex.RUnlock()
+	fmt.Fprintf(w, "Temperature = %v*C, Humidity = %v%%", temperature, humidity)
+}
 
-	if err != nil {
-		fmt.Fprintf(w, "Error occured during temp readout: %s", err)
-	} else {
-		fmt.Fprintf(w, "Temperature = %v*C, Humidity = %v%%", temperature, humidity)
-	}
+func startLoop() {
+	go func() {
+		for {
+			err := readTemperature()
+
+			if err != nil {
+				log.Printf("Error during temperature readot: %s\n", err)
+			}
+
+			time.Sleep(time.Minute)
+		}
+	}()
 }
 
 func main() {
 	router := httprouter.New()
 	router.GET("/", temperatureHandler)
+
+	startLoop()
 
 	log.Println("Ready to serve!")
 	log.Fatal(http.ListenAndServe(":8080", router))
